ea3fa409c2cae453270713951297e685fd99a266
Renjith Pillai <renjith.pillai@sap.com>
2025-04-08 18:34:10 +0530

    State #1

--- run_analysis.sh
+++ run_analysis.sh
@@ -6,8 +6,9 @@ export GITHUB_USERNAME="i306570"
 export GITHUB_ENTERPRISE_URL="https://github.wdf.sap.corp"
 export GITHUB_IS_ENTERPRISE="true"  # Set to "true" if using GitHub Enterprise
 export GITHUB_START_DATE="2025-01-01"
-export GITHUB_END_DATE="2024-03-28"
+export GITHUB_END_DATE="2025-03-28"
 export GITHUB_VERIFY_SSL=False
+
 
 # Create virtual environment if it doesn't exist
 if [ ! -d "venv" ]; then
@@ -19,6 +20,7 @@ fi
 source venv/bin/activate
 
 # Install required packages
+pip install --upgrade pip
 pip install requests
 
 # Run the analysis script
